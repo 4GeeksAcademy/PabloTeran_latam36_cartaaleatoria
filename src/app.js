371493e79995e9c9a40b8ba0feb9a1df00a6a548
Pablo Teran <144842209+Devnaret0399@users.noreply.github.com>
2024-10-07 22:30:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 function generateRandomCard() {
-  // Palos disponibles
   const suits = ["heart", "spade", "club", "diamond"];
-  // Valores disponibles
   const values = [
     "A",
     "2",
@@ -18,34 +16,28 @@ function generateRandomCard() {
     "K"
   ];
 
-  // Generar un palo y valor aleatorio
   const randomSuit = suits[Math.floor(Math.random() * suits.length)];
   const randomValue = values[Math.floor(Math.random() * values.length)];
 
-  // Seleccionar el contenedor de la carta
   const cardContainer = document.getElementById("card-container");
 
   if (!cardContainer) {
     return;
   }
 
-  // Limpiar las clases anteriores
   cardContainer.classList.remove("heart", "spade", "club", "diamond");
 
-  // Añadir la clase del palo y actualizar el contenido
   cardContainer.classList.add(randomSuit);
 
-  // Actualizar los elementos dentro de la carta
   cardContainer.querySelector(".top-suit").innerHTML = getSuitSymbol(
     randomSuit
-  ); // Solo el símbolo en la esquina
-  cardContainer.querySelector(".middle-suit").innerHTML = randomValue; // El valor en el centro
+  );
+  cardContainer.querySelector(".middle-suit").innerHTML = randomValue;
   cardContainer.querySelector(".bottom-suit").innerHTML = getSuitSymbol(
     randomSuit
-  ); // Solo el símbolo en la esquina
+  );
 }
 
-// Función para obtener el símbolo del palo
 function getSuitSymbol(suit) {
   switch (suit) {
     case "heart":
